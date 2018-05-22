6e6232ed84ed3cd144070704cc6a988d13da3bcc
ZouXin6 <ZouXin6@huaqin.com>
2018-05-22 10:27:01 +0800

    lidong,bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -14,13 +15,20 @@ def run_game():
         (ai_settings.screen_width,ai_settings.screen_height))
 
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+
     # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
+
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
 
     # 开始游戏主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
+        # 更新子弹状态
+        gf.update_bullets(bullets)
+        # 更新屏幕上的图像
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- ship.py
+++ ship.py
@@ -4,9 +4,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         """初始化飞船并设置其初始位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # 加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('images/ship.bmp')
@@ -17,6 +18,25 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # 在飞船的属性center中存储小数值
+        self.center = float(self.rect.centerx)
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
     def blitme(self):
         """在指定位置绘制飞船"""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        if self.moving_right:
+            if self.moving_right and self.rect.right < self.screen_rect.right:
+                self.center += self.ai_settings.ship_speed_factor
+        elif self.moving_left:
+            if self.moving_left and self.rect.left > 0:
+                self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
